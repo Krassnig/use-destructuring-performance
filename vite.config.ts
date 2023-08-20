b16b7baaa79a65e10c99076711d6823ca09605fb
Niklas Kraßnig <24553082+Krassnig@users.noreply.github.com>
2023-08-20 12:40:01 +0200

    minor stuff

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './' // relative pathing for assets because this project is hosted at a relative path https://krassnig.github.io/use-destructuring-performance/
 })
